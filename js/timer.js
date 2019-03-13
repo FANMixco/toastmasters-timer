b0db61c581745a9e27db0bdb5e74dad85acc3de0
Federico Navarrete <darklord.navarrete@gmail.com>
2019-03-13 16:51:56 +0100

    Update timer.js
    Added delay for re-scaling.

--- js/timer.js
+++ js/timer.js
@@ -241,8 +241,10 @@ function resizeScreen(){
     $('#superContainer').css("height", `${window.innerHeight}px`);
     $('#superContainer').css("transform-origin", "50% 0% 0px");
     
-    let scaleVal = window.innerHeight / 600;
-    $('#superContainer').css("transform", `scale(${scaleVal}`);
+    setTimeout(function() {
+		let scaleVal = window.innerHeight / 600;
+		$('#superContainer').css("transform", `scale(${scaleVal}`);
+    }, 100);
     
     setTimeout(function() {
         if ($(".mdl-textfield__input")[0].getBoundingClientRect().width > $('body').outerWidth()){
