c12a404e95d48d383634e25aecb9516ca6d339fe
Federico Navarrete <fanmixco@gmail.com>
2021-09-05 11:48:03 +0200

    Fixed language issue

--- js/lib/translate.js
+++ js/lib/translate.js
@@ -5,6 +5,10 @@ let lngObject;
 function load() {
     let translate = new Translate();
     lang = navigator.language || navigator.userLanguage;
+
+    if (lang.includes('-'))
+        lang = lang.split('-')[0];
+
     if (!langs.includes(lang))
         lang = "en";
     translate.init(lang);
