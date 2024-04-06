21ca4aef127d5def50fb91656ba87a4d7b201a6a
Federico Navarrete <fanmixco@gmail.com>
2024-04-06 11:42:27 +0200

    Cleaned a bit

---
+++ js/lib/extras.js
@@ -0,0 +1,9 @@
+//Translator
+load();
+
+//Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-139701351-1');
