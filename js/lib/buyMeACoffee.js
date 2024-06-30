95b4b8bdbb3c991544235e8d5600a10998b692fd
Federico Navarrete <fanmixco@gmail.com>
2024-06-30 20:56:55 +0200

    Odd bugs

--- js/lib/buyMeACoffee.js
+++ js/lib/buyMeACoffee.js
@@ -1,21 +1,24 @@
-setTimeout(() => {
+document.addEventListener('DOMContentLoaded', () => {
     let buy_me_coffee = document.createElement('script');
-
     buy_me_coffee.setAttribute('src', 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js');
     buy_me_coffee.dataset.name = "BMC-Widget";
     buy_me_coffee.dataset.cfasync = "false";
     buy_me_coffee.dataset.id = "fanmixco";
     try {
         buy_me_coffee.dataset.description = lngObject.buyMeACoffee;
-        buy_me_coffee.dataset.message = lngObject.buyMeACoffeeMsg;    
+        buy_me_coffee.dataset.message = lngObject.buyMeACoffeeMsg;
     } catch {
         buy_me_coffee.dataset.description = "Support me on Buy me a coffee!";
-        buy_me_coffee.dataset.message = "Empower me to improve this incredible app! Buy me a coffee!";    
+        buy_me_coffee.dataset.message = "Empower me to improve this incredible app! Buy me a coffee!";
     }
     buy_me_coffee.dataset.color = "#5F7FFF";
     buy_me_coffee.dataset.position = "Right";
     buy_me_coffee.dataset.x_margin = "18";
     buy_me_coffee.dataset.y_margin = "18";
 
+    buy_me_coffee.onload = () => {
+        console.log('Buy Me A Coffee widget loaded successfully.');
+    };
+
     document.body.appendChild(buy_me_coffee);
-}, 1500);
+});
